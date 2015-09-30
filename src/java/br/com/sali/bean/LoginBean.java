1ca99ecba7b00c731aa397c7e47105e15bd9ccbf
JoseMarcio <Jos@JM-PC>
2015-09-30 00:30:08 -0300

    Recuperar

--- src/java/br/com/sali/bean/LoginBean.java
+++ src/java/br/com/sali/bean/LoginBean.java
@@ -1,8 +1,11 @@
 package br.com.sali.bean;
 
 import br.com.sali.regras.InstituicaoRN;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
 
 /**
  * Managed Bean Login.
@@ -12,9 +15,29 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean(name = "loginBean")
 @RequestScoped
 public class LoginBean {
+    
+    private String emailInformado;
+
+    public String getEmailInformado() {
+        return emailInformado;
+    }
+
+    public void setEmailInformado(String emailInformado) {
+        this.emailInformado = emailInformado;
+    }
+    
+        
+    
 
     public void carregarInstituicao() {
         InstituicaoRN instituicaoRN = new InstituicaoRN();
+    }
+    
+    
+    public void recuperarEmail(){
+    
+        RequestContext fc = RequestContext.getCurrentInstance();
+        fc.showMessageInDialog(new FacesMessage(emailInformado));
     }
     
     
